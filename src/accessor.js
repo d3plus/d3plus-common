f59c059a25af64ee0c5c729c8386dfd3667965d7
Dave Landry <landry.dave@gmail.com>
2016-05-10 15:03:40 -0400

    fixes accessor example doc

--- src/accessor.js
+++ src/accessor.js
@@ -3,7 +3,7 @@
     @desc Wraps an object key in a simple accessor function.
     @param {String} key The key to be returned from each Object passed to the function.
     @example <caption>this</caption>
-constant("id");
+accessor("id");
     @example <caption>returns this</caption>
 function(d) {
   return d["id"];
