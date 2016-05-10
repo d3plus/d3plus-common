c8ce4342b34d658013bf7422201c27711d019936
Dave Landry <landry.dave@gmail.com>
2016-05-10 14:46:52 -0400

    adds d3 modules to buble exclude

--- .rollup.full.js
+++ .rollup.full.js
@@ -9,5 +9,5 @@ export default {
   globals: function(id) { return id.replace(/-/g, "_"); },
   moduleId: "d3plus-common",
   moduleName: "d3plus_common",
-  plugins: [json(), deps({"jsnext": true}), buble()]
+  plugins: [json(), deps({"jsnext": true}), buble({exclude: "node_modules/d3-*/**"})]
 };
