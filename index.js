8b31f2545fdf1860e5963f8cdf10a17645b2a17e
Dave Landry <landry.dave@gmail.com>
2016-05-10 15:26:35 -0400

    adds colorNest function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 export {version} from "./package.json";
 
 export {default as accessor} from "./src/accessor";
+export {default as colorNest} from "./src/colorNest";
 export {default as constant} from "./src/constant";
+export {default as merge} from "./src/merge";
