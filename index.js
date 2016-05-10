3646a742549a824a5b6040b40109aae4df503706
Dave Landry <landry.dave@gmail.com>
2016-05-10 15:45:01 -0400

    adds getSize function

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ export {version} from "./package.json";
 export {default as accessor} from "./src/accessor";
 export {default as colorNest} from "./src/colorNest";
 export {default as constant} from "./src/constant";
+export {default as getSize} from "./src/getSize";
 export {default as merge} from "./src/merge";
---
+++ test/getSize.js
@@ -0,0 +1,22 @@
+import {test} from "tape";
+import {select} from "d3-selection";
+import {default as getSize} from "../src/getSize.js";
+
+test("getSize", (assert) => {
+
+  let div = select("body").append("div"),
+      size = getSize(div.node());
+
+  assert.true(size[0] === 784 && size[1] === 562, "Window Detection");
+
+  div = div.style("width", "300px").style("height", "200px").append("div");
+  size = getSize(div.node());
+  assert.true(size[0] === 300 && size[1] === 200, "Parent Styles");
+
+  size = getSize(div.style("width", "200px").style("height", "100px").node());
+  assert.true(size[0] === 200 && size[1] === 100, "Inline Styles");
+
+  div.remove();
+  assert.end();
+
+});
