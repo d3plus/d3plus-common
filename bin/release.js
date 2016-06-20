e979feef8258fbaa4404be82723037c63d0a67a6
Dave Landry <landry.dave@gmail.com>
2016-06-19 22:52:42 -0400

    fixes release script

--- bin/release.js
+++ bin/release.js
@@ -1,5 +1,4 @@
 #! /usr/bin/env node
-
 // export GITHUB_TOKEN=xxx
 // git config --global credential.helper osxkeychain
 
@@ -12,35 +11,39 @@ const release = require("grizzly"),
 let minor = version.split(".");
 minor = minor.slice(0, minor.length - 1).join(".");
 
-// shell.exec("d3plus-test");
-// shell.exec("d3plus-docs");
+shell.exec("d3plus-test");
+shell.exec("d3plus-docs");
 
 const changelog = shell.exec("git log --pretty=format:'* %s (%h)' `git describe --tags --abbrev=0`...HEAD", {"silent": true}).stdout;
 
 rollup().then(() => {
   rollup({"deps": true}).then(() => {
-    // shell.exec(`uglifyjs build/${name}.js -c -m -o build/${name}.min.js`);
-    // shell.exec(`uglifyjs build/${name}.full.js -c -m -o build/${name}.full.min.js`);
-    // shell.exec(`rm -f build/${name}.zip && zip -j -q build/${name}.zip -- LICENSE README.md build/${name}.js build/${name}.min.js build/${name}.full.js build/${name}.full.min.js`);
+    shell.exec(`uglifyjs build/${name}.js -c -m -o build/${name}.min.js`);
+    shell.exec(`uglifyjs build/${name}.full.js -c -m -o build/${name}.full.min.js`);
+    shell.exec(`rm -f build/${name}.zip && zip -j -q build/${name}.zip -- LICENSE README.md build/${name}.js build/${name}.min.js build/${name}.full.js build/${name}.full.min.js`);
     shell.exec("git add *");
     shell.exec(`git commit -m \"compiles v${version}\"`);
     shell.exec("git push -q");
-    // shell.exec("npm publish ./");
-    // shell.exec(`git tag v${version} && git push -q --tags`);
-    // release(token, {
-    //   "repo": name,
-    //   "owner": "d3plus",
-    //   "tag_name": `v${version}`,
-    //   "name": `v${version}`,
-    //   "body": changelog
-    // }, (error) => {
-    //   if (error) console.error(error.message);
-    // });
-    // shell.cp(`build/${name}.js`, `../d3plus-website/js/${name}.v${minor}.js`);
-    // shell.cp(`build/${name}.full.js`, `../d3plus-website/js/${name}.v${minor}.full.js`);
-    // shell.cp(`build/${name}.min.js`, `../d3plus-website/js/${name}.v${minor}.min.js`);
-    // shell.cp(`build/${name}.full.min.js`, `../d3plus-website/js/${name}.v${minor}.full.min.js`);
-    // shell.cd("../d3plus-website");
-    // shell.exec(`git add * && git commit -m \"${name} v${version}\" && git push -q && cd -`);
+    shell.exec("npm publish ./");
+    shell.exec(`git tag v${version}`);
+    shell.exec("git push -q --tags");
+    release(token, {
+      "repo": name,
+      "owner": "d3plus",
+      "tag_name": `v${version}`,
+      "name": `v${version}`,
+      "body": changelog
+    }, (error) => {
+      if (error) console.error(error.message);
+    });
+    shell.cp(`build/${name}.js`, `../d3plus-website/js/${name}.v${minor}.js`);
+    shell.cp(`build/${name}.full.js`, `../d3plus-website/js/${name}.v${minor}.full.js`);
+    shell.cp(`build/${name}.min.js`, `../d3plus-website/js/${name}.v${minor}.min.js`);
+    shell.cp(`build/${name}.full.min.js`, `../d3plus-website/js/${name}.v${minor}.full.min.js`);
+    shell.cd("../d3plus-website");
+    shell.exec("git add *");
+    shell.exec(`git commit -m \"${name} v${version}\"`);
+    shell.exec("git push -q");
+    shell.cd("-");
   });
 });
