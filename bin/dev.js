590165a0ec6ee78b6ca430bb5cb670b4390d493d
Dave Landry <landry.dave@gmail.com>
2016-06-23 13:58:33 -0400

    adds live browser reload to dev server

--- bin/dev.js
+++ bin/dev.js
@@ -1,18 +1,9 @@
 #! /usr/bin/env node
 
-const rollup = require("./rollup");
-const shell = require("shelljs");
-const server = require("node-static");
+require("live-server").start({
+  "noBrowser": true,
+  "port": 4000,
+  "watch": ["build", "test"]
+});
 
-const file = new server.Server();
-
-shell.echo("creating static fileserver at http://127.0.0.1:4000");
-require("http").createServer((request, response) => {
-  request.addListener("end", () => {
-    const d = new Date();
-    shell.echo(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} serving ${request.url}`);
-    file.serve(request, response);
-  }).resume();
-}).listen(4000);
-
-rollup({"deps": true, "watch": true});
+require("./rollup")({"deps": true, "watch": true});
