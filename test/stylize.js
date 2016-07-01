edce28978f9f93759f2a3c3d4d9b977a26e62717
Dave Landry <landry.dave@gmail.com>
2016-07-01 11:41:18 -0400

    adds new stylize method

---
+++ test/stylize.js
@@ -0,0 +1,20 @@
+import {test} from "tape";
+import {default as stylize} from "../src/stylize.js";
+import {select} from "d3-selection";
+
+test("stylize", (assert) => {
+
+  const styles = {
+    "color": "red",
+    "font-size": "12px"
+  };
+
+  const div = select("body").append("div");
+
+  stylize(div, styles);
+
+  assert.equals(div.style("color"), "rgb(255, 0, 0)", "Basic Style");
+  assert.equals(div.style("font-size"), "12px", "Hyphenated Style");
+  assert.end();
+
+});
