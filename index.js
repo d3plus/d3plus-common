3afd383d9f1fbd61dafb2bf0db61538fb054ab2d
Dave Landry <landry.dave@gmail.com>
2016-09-09 13:42:38 -0400

    correctly exports closest function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export {default as accessor} from "./src/accessor";
 export {default as attrize} from "./src/attrize";
 export {default as BaseClass} from "./src/BaseClass";
+export {default as closest} from "./src/closest";
 export {default as constant} from "./src/constant";
 export {default as elem} from "./src/elem";
 export {default as merge} from "./src/merge";
