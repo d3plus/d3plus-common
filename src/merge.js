83379c30ec30e136676d2209d2eb5711f5864390
Dave Landry <landry.dave@gmail.com>
2016-09-09 12:59:23 -0400

    adds custom aggregations to merge function

--- src/merge.js
+++ src/merge.js
@@ -5,6 +5,7 @@ import {keys} from "d3-collection";
     @function merge
     @desc Combines an Array of Objects together and returns a new Object.
     @param {Array} objects The Array of objects to be merged together.
+    @param {Object} aggs An object containing specific aggregation methods (functions) for each key type. By default, numbers are summed and strings are returned as an array of unique values.
     @example <caption>this</caption>
 merge([
   {id: "foo", group: "A", value: 10},
@@ -13,7 +14,7 @@ merge([
     @example <caption>returns this</caption>
 {id: ["bar", "foo"], group: "A", value: 30}
 */
-export default function(objects) {
+export default function(objects, aggs = {}) {
 
   const availableKeys = new Set(merge(objects.map(o => keys(o)))),
         newObject = {};
@@ -21,8 +22,9 @@ export default function(objects) {
   availableKeys.forEach(k => {
     const values = objects.map(o => o[k]);
     let value;
-    if (values.map(v => typeof v).indexOf("string") >= 0) {
-      value = Array.from(new Set(values).values());
+    if (aggs[k]) value = aggs[k](values);
+    else if (values.map(v => typeof v).indexOf("string") >= 0) {
+      value = Array.from(new Set(values));
       if (value.length === 1) value = value[0];
     }
     else value = sum(values);
--- test/merge.js
+++ test/merge.js
@@ -4,13 +4,14 @@ import {default as merge} from "../src/merge.js";
 test("merge", assert => {
 
   const obj = merge([
-    {id: "foo", group: "A", value: 10},
-    {id: "bar", group: "A", value: 20}
-  ]);
+    {id: "foo", group: "A", value: 10, agg: 1},
+    {id: "bar", group: "A", value: 20, agg: 1}
+  ], {agg: a => a[0]});
 
   assert.equals(obj.group, "A", "Unique String");
   assert.true(obj.id[0] === "foo" && obj.id[1] === "bar", "Multiple Strings");
   assert.equals(obj.value, 30, "Number Summation");
+  assert.equals(obj.agg, 1, "Custom Aggregation");
   assert.end();
 
 });
