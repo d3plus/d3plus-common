5a71eb6f7b74c36301c667717b6e61ad9278fa26
Dave Landry <landry.dave@gmail.com>
2016-10-27 09:52:46 -0400

    moves bin scripts to d3plus-dev

--- bin/banner.js
+++
@@ -1,9 +0,0 @@
-const shell = require("shelljs"),
-      {description, homepage, license, name, version} = JSON.parse(shell.cat("package.json"));
-
-module.exports = `/*
-  ${name} v${version}
-  ${description}
-  Copyright (c) ${new Date().getFullYear()} D3plus - ${homepage}
-  @license ${license}
-*/`;
--- bin/locale.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-const shell = require("shelljs");
-shell.exec("i18next .. -r -l en_US,es_ES -f 'locale.t' --fileFilter '*.js' --directoryFilter 'd3plus-*,src' -o src/locales");
-shell.echo("Compiled translations.");
