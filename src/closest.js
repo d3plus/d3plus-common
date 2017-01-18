3bd60cd47eee5e9ba8366342fb516f182502f259
Dave Landry <landry.dave@gmail.com>
2017-01-18 07:38:55 -0500

    catches undefined/empty array in closest function (closes #63)

--- src/closest.js
+++ src/closest.js
@@ -4,6 +4,7 @@
     @param {Number} n The number value to use when searching the array.
     @param {Array} arr The array of values to test against.
 */
-export default function(n, arr) {
+export default function(n, arr = []) {
+  if (!arr || !(arr instanceof Array) || !arr.length) return undefined;
   return arr.reduce((prev, curr) => Math.abs(curr - n) < Math.abs(prev - n) ? curr : prev);
 }
--- test/closest.js
+++ test/closest.js
@@ -5,6 +5,8 @@ test("closest", assert => {
 
   assert.equals(closest(9, [0, 10]), 10, "Closest Over");
   assert.equals(closest(1, [0, 10]), 0, "Closest Under");
+  assert.equals(closest(1, undefined), void 0, "Undefined");
+  assert.equals(closest(1, []), void 0, "Empty Array");
   assert.end();
 
 });
