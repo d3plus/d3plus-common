c38fc9e87889d3eafdd344dd8c8114bcb3c57995
Dave Landry <landry.dave@gmail.com>
2017-05-08 13:09:05 -0400

    adds explicit "d3plus" namespace to locale init

--- src/locale.js
+++ src/locale.js
@@ -5,7 +5,10 @@ import esES from "./locales/es-ES/translation.json";
 
 export default i18next.init({
   fallbackLng: "en-US",
+  defaultNS: "d3plus",
+  fallbackNS: "d3plus",
   initImmediate: false,
+  ns: "d3plus",
   resources: {
     "en-US": {translation: enUS},
     "es-ES": {translation: esES}
