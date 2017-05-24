2c210623de35192434df0c4b29803315b6a3e59b
Dave Landry <landry.dave@gmail.com>
2017-05-24 12:23:08 -0400

    adds global configPrep function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ export {default as assign} from "./src/assign";
 export {default as attrize} from "./src/attrize";
 export {default as BaseClass} from "./src/BaseClass";
 export {default as closest} from "./src/closest";
+export {default as configPrep} from "./src/configPrep";
 export {default as constant} from "./src/constant";
 export {default as elem} from "./src/elem";
 export {default as isObject} from "./src/isObject";
---
+++ src/configPrep.js
@@ -0,0 +1,64 @@
+import assign from "./assign";
+
+/**
+    @function configPrep
+    @desc Preps a config object for d3plus data, and optionally bubbles up a specific nested type. When using this function, you must bind a d3plus class' `this` context.
+    @param {Object} [config = this._shapeConfig] The configuration object to parse.
+    @param {String} [type = "shape"] The event classifier to user for "on" events. For example, the default event type of "shape" will apply all events in the "on" config object with that key, like "click.shape" and "mouseleave.shape", in addition to any gloval events like "click" and "mouseleave".
+    @param {String} [nest] An optional nested key to bubble up to the parent config level.
+*/
+export default function configPrep(config = this._shapeConfig, type = "shape", nest = false) {
+
+  let newConfig = {duration: this._duration, on: {}};
+
+  const wrapFunction = func => (d, i, s) => {
+    while (d.__d3plus__ && d.data) {
+      i = d.i;
+      d = d.data;
+    }
+    return func(d, i, s);
+  };
+
+  const parseEvents = (newObj, on) => {
+
+    for (const event in on) {
+
+      if ({}.hasOwnProperty.call(on, event) && !event.includes(".") || event.includes(`.${type}`)) {
+
+        newObj.on[event] = wrapFunction(on[event]);
+
+      }
+
+    }
+
+  };
+
+  const keyEval = (newObj, obj) => {
+
+    for (const key in obj) {
+
+      if ({}.hasOwnProperty.call(obj, key)) {
+
+        if (key === "on") parseEvents(newObj, obj[key]);
+        else if (typeof obj[key] === "function") {
+          newObj[key] = wrapFunction(obj[key]);
+        }
+        else if (typeof obj[key] === "object" && !(obj instanceof Array)) {
+          newObj[key] = {};
+          keyEval(newObj[key], obj[key]);
+        }
+        else newObj[key] = obj[key];
+
+      }
+
+    }
+
+  };
+
+  keyEval(newConfig, config);
+  if (this._on) parseEvents(newConfig, this._on);
+
+  if (nest && config[nest]) newConfig = assign(newConfig, config[nest]);
+  return newConfig;
+
+}
