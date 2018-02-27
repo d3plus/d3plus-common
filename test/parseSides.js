a1f647d275026a57608cfb457f47421ecabf2d5e
Nick Bond <bond.n@husky.neu.edu>
2018-02-27 12:54:03 -0500

    allow parseSides function to accept a number as an argument

--- test/parseSides.js
+++ test/parseSides.js
@@ -10,6 +10,7 @@ test("parseSides", assert => {
   assert.deepEqual(parseSides("10 20 30 40"), {top: 10, right: 20, bottom: 30, left: 40}, "Four value");
   assert.deepEqual(parseSides("10 20 30 40 50"), {top: 10, right: 20, bottom: 30, left: 40}, "More than four values");
   assert.deepEqual(parseSides("10px 20px 30px"), {top: 10, right: 20, bottom: 30, left: 20}, "px values");
+  assert.deepEqual(parseSides(40), {top: 40, right: 40, bottom: 40, left: 40}, "number value");
 
 });
 
