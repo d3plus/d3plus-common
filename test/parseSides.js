0cc10195a1b9ec150a0bb44a458bb8e2cd169a55
Nick Bond <bond.n@husky.neu.edu>
2018-02-27 12:16:19 -0500

    default value to 0 if no value supplied

--- test/parseSides.js
+++ test/parseSides.js
@@ -4,7 +4,7 @@ import {default as parseSides} from "../src/parseSides.js";
 export default zora()
     .test("parseSides", assert => {
 
-      assert.equal(parseSides(""), {top: 0, right: 0, bottom: 0, left: 0}, "No values");
+      assert.deepEqual(parseSides(""), {top: 0, right: 0, bottom: 0, left: 0}, "No values");
       assert.deepEqual(parseSides("10"), {top: 10, right: 10, bottom: 10, left: 10}, "Single value");
       assert.deepEqual(parseSides("10 20"), {top: 10, right: 20, bottom: 10, left: 20}, "Two values");
       assert.deepEqual(parseSides("10 20 30"), {top: 10, right: 20, bottom: 30, left: 20}, "Three values");
