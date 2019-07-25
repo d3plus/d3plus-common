433eab1bca8eb11e788ade83cbf1a005f2916f3f
Dave Landry <landry.dave@gmail.com>
2019-07-25 13:03:53 -0400

    fixes error in findLocale

--- src/findLocale.js
+++ src/findLocale.js
@@ -19,7 +19,7 @@ export default function(locale) {
   if (typeof locale !== "string" || locale.length === 5) return locale;
   if (defaultLocales[locale]) return defaultLocales[locale];
   const list = locales.filter(d => d["iso639-1"] === locale);
-  if (!list.length) return _;
+  if (!list.length) return locale;
   else if (list.length === 1) return list[0].tag;
   else if (list.find(d => d.tag === `${locale}-${locale.toUpperCase()}`)) return `${locale}-${locale.toUpperCase()}`;
   else return list[0].tag;
