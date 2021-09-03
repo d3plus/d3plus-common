aaadea2f3ba2aa2054562e8fe79b1d5dff43bcac
Dave Landry <landry.dave@gmail.com>
2021-09-03 14:02:57 -0400

    adds accessor argument to unique function

--- src/unique.js
+++ src/unique.js
@@ -1,12 +1,14 @@
 /**
     @function unique
     @desc ES5 implementation to reduce an Array of values to unique instances.
-    @param {Array} objects The Array of objects to be filtered.
+    @param {Array} arr The Array of objects to be filtered.
+    @param {Function} [accessor] An optional accessor function used to extract data points from an Array of Objects.
     @example <caption>this</caption>
 unique(["apple", "banana", "apple"]);
     @example <caption>returns this</caption>
 ["apple", "banana"]
 */
-export default function(arr) {
-  return arr.filter((k, i, a) => a.indexOf(k) === i);
+export default function(arr, accessor = d => d) {
+  const values = arr.map(accessor);
+  return arr.filter((d, i) => values.indexOf(accessor(d)) === i);
 }
