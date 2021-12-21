77418df236d068236c06a0ca450d46d1a7dfc169
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-12-21 09:56:25 -0300

    adds pt-BR locale

---
+++ src/locales/pt-BR.js
@@ -0,0 +1,16 @@
+export default {
+  "and": "e",
+  "Back": "Voltar",
+  "Click to Expand": "Aperte aqui para Expandir",
+  "Click to Hide": "Aperte aqui para Ocultar",
+  "Click to Highlight": "Aperte aqui para Destacar",
+  "Click to Reset": "Aperte aqui para Resetar",
+  "Download": "Baixar",
+  "Loading Visualization": "Carregando Visualização",
+  "No Data Available": "Dados Não Disponíveis",
+  "Powered by D3plus": "Funciona com D3plus",
+  "Share": "Porcentagem",
+  "Shift+Click to Hide": "Shift+Clique para Ocultar",
+  "Total": "Total",
+  "Values": "Valores"
+};
