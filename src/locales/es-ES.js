e0c7b7e6fe5220e127bd327de5ed000a2a1f4420
Dave Landry <landry.dave@gmail.com>
2022-01-21 16:13:13 -0500

    updates es and pt translations for new d3plus-viz legend behavior

--- src/locales/es-ES.js
+++ src/locales/es-ES.js
@@ -4,13 +4,15 @@ export default {
   "Click to Expand": "Clic para Ampliar",
   "Click to Hide": "Clic para Ocultar",
   "Click to Highlight": "Clic para Resaltar",
-  "Click to Reset": "Clic para Restablecer",
+  "Click to Show": "Clic para Mostrar",
+  "Click to Show All": "Clic para Mostrar Todo",
   "Download": "Descargar",
   "Loading Visualization": "Cargando Visualización",
   "No Data Available": "Datos No Disponibles",
   "Powered by D3plus": "Funciona con D3plus",
   "Share": "Porcentaje",
   "Shift+Click to Hide": "Mayús+Clic para Ocultar",
+  "Shift+Click to Highlight": "Mayús+Clic para Resaltar",
   "Total": "Total",
   "Values": "Valores"
 };
--- src/locales/pt-BR.js
+++ src/locales/pt-BR.js
@@ -1,16 +1,18 @@
 export default {
   "and": "e",
   "Back": "Voltar",
-  "Click to Expand": "Aperte aqui para Expandir",
-  "Click to Hide": "Aperte aqui para Ocultar",
-  "Click to Highlight": "Aperte aqui para Destacar",
-  "Click to Reset": "Aperte aqui para Resetar",
+  "Click to Expand": "Clique para Expandir",
+  "Click to Hide": "Clique para Ocultar",
+  "Click to Highlight": "Clique para Destacar",
+  "Click to Show": "Clique para Mostrar",
+  "Click to Show All": "Clique para Mostrar Tudo",
   "Download": "Baixar",
   "Loading Visualization": "Carregando Visualização",
   "No Data Available": "Dados Não Disponíveis",
   "Powered by D3plus": "Funciona com D3plus",
   "Share": "Porcentagem",
   "Shift+Click to Hide": "Shift+Clique para Ocultar",
+  "Shift+Click to Highlight": "Shift+Clique para Destacar",
   "Total": "Total",
   "Values": "Valores"
 };
