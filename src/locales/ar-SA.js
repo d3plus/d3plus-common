ba333bab9f48a50d89ef30a5a89a3f9d42b55ee0
scespinoza <secancinoe@gmail.com>
2023-10-17 12:47:01 -0300

    adds arabic common translations

---
+++ src/locales/ar-SA.js
@@ -0,0 +1,19 @@
+export default {
+    "and": "و",
+    "Back": "للخلف",
+    "Click to Expand": "انقر للتوسيع",
+    "Click to Hide": "اضغط للإخفاء",
+    "Click to Highlight": "اضغط للتحديد",
+    "Click to Show": "انقر للعرض",
+    "Click to Show All": "انقر لعرض الكل",
+    "Download": "تحميل",
+    "Loading Visualization": "جاري تحميل التصوير البياني",
+    "No Data Available": "لا تتوفر بيانات",
+    "Powered by D3plus": "مدعوم بواسطة D3plus",
+    "Share": "مشاركة",
+    "Shift+Click to Hide": "Shift+انقر للإخفاء",
+    "Shift+Click to Highlight": "Shift + للتحديد اضغط",
+    "Total": "المجموع",
+    "Values": "القيم"
+  };
+  
--- src/locales/index.js
+++ src/locales/index.js
@@ -1,7 +1,9 @@
 import esES from "./es-ES.js";
 import ptBR from "./pt-BR.js";
+import arSA from "./ar-SA.js";
 
 export default {
   "es-ES": esES,
-  "pt-BR": ptBR
+  "pt-BR": ptBR,
+  "ar-SA": arSA,
 };
